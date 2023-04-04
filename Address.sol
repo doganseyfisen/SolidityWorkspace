57cf03f46dec86c757bdf58c1e00c122eb7d9614
Doğan Seyfi Şen <118891768+doganseyfisen@users.noreply.github.com>
2023-04-04 04:01:23 +0300

    Create Address.sol

---
+++ Address.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract Address{
+    address private owner = msg.sender;
+    function getOwner() public view returns(address){
+        return owner;
+    }
+}
